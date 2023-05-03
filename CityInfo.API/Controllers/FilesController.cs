2c4647b3ae9a52c56de7878ebaa824cf821f28be
Wilbert Holkema <wilbert@dsesoftware.nl>
2023-05-03 09:57:27 +0200

    Initial upload

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileid}")]
+        public ActionResult GetFile(string fileId)
+        {
+            fileId = "Dag1v1.pdf";
+
+            if (!System.IO.File.Exists(fileId))
+            {
+                return NotFound();
+            }
+
+            if(!_fileExtensionContentTypeProvider.TryGetContentType(fileId, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(fileId);
+            return File(bytes, contentType, Path.GetFileName(fileId));
+        }
+    }
+}
---
+++ CityInfo.API/Services/LocalMailService.cs
@@ -0,0 +1,22 @@
+﻿namespace CityInfo.API.Services
+{
+    public class LocalMailService : IMailService
+    {
+        private readonly string _mailTo = String.Empty;
+        private readonly string _mailFrom = String.Empty;
+
+        public LocalMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
+
+        public void Send(string subject, string message)
+        {
+            Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with {nameof(LocalMailService)}.");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Message: {message}");
+        }
+
+    }
+}
